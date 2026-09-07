abbb50c528b6fd04fca853489996d43000d37a64
ThomasHunt2025 <th24074@students.sacredheart.school.nz>
2026-09-07 03:02:18 +0000

    bottle code

--- app.py
+++ app.py
@@ -1,4 +1,28 @@
 from bottle import run, route, template, view, static_file 
 
-@route
+@route('/')
+def h():
+    return template('home.tpl')
+
+@route('/problematic')
+def prob():
+    return template('problematic.tpl')
+
+@route('teens')
+def teens():
+    return template('NZTeens.tpl')
+
+@route('/impacts')
+def imp():
+    return template('impacts.tpl')
+
+@route('/static/<filename>')
+def server_static(filename):
+    return static_file(filename, root='./static')
+
+run(host='localhost', port='8080', debug=True, reloader=True)
+
+
+
+
 
